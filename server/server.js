08d49fec42afecfdf8bcd47766e35fde56361946
TheoAnastasiadis <theoanastasiadis98@gmail.com>
2018-07-21 15:33:16 +0300

    Getting it heroku ready

--- server/server.js
+++ server/server.js
@@ -5,8 +5,10 @@ const publicPath = path.join(__dirname, '../public');
 
 const app = express()
 
-app.use(express.static(publicPath)
+app.use(express.static(publicPath))
 
-app.listen(3000, () => {
-    console.log('Server running on 3000...')
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Server running on ${port}...`)
 })
