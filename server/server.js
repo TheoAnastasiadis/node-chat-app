625407555ee09ded212f70bea435534f5ff895bc
TheoAnastasiadis <theoanastasiadis98@gmail.com>
2018-07-21 15:46:57 +0300

    Installed socket.io

--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname, '../public');
 
 const app = express()
+const server = http.createServer(app)
+const io = socketIO(server)
 
 app.use(express.static(publicPath))
 
+io.on('connection', (socket) => {
+    console.log('New user connected!')
+})
+
 
 const port = process.env.PORT || 3000
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server running on ${port}...`)
 })
